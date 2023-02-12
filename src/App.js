8af06a723ac7d0c154fcae4d1a498815b802729a
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-02-13 07:44:48 +1100

    Try building components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import WeatherPic from "./WeatherPic.js";
+import InfoPanel from "./InfoPanel.js";
+import Attribution from "./Attribution.js";
+
 import "./App.css";
+import "./WeatherPic.css";
+import "./InfoPanel.css";
+import "./Attribution.css";
 
-export default App;
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <div className="main-container"></div>
+      Test
+      <WeatherPic />
+      <InfoPanel />
+      <Attribution />
     </div>
   );
 }
---
+++ src/InfoPanel.js
@@ -0,0 +1,21 @@
+/*
+import React from "react";
+
+import Search from "./Search.js";
+import Results from "./Results.js";
+import GeoInfo from "./GeoInfo.js";
+
+import "./Search.css";
+import "./Results.css";
+import "./GeoInfo.css";
+
+export default function InfoPanel() {
+  return (
+    <div className="">
+      <Search />
+      <Results />
+      <GeoInfo />
+    </div>
+  );
+}
+*/
