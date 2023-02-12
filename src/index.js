cd2dc60610476a7624cff25bf0d42cb92d559ef1
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-02-13 07:49:05 +1100

    Trial fixing

--- src/index.js
+++ src/index.js
@@ -10,3 +10,18 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+/*
+
+import "./App.css";
+
+export default App;
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main-container"></div>
+    </div>
+  );
+}
+*/
