34038b88f6e78b46c36bac414040b1f70264dedd
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-02-15 17:09:55 +1100

    Put info panel inside pic panel

--- src/WeatherPic.js
+++ src/WeatherPic.js
@@ -5,8 +5,7 @@ import "./WeatherPic.css";
 export default function WeatherPic() {
   return (
     <div className="WeatherPic">
-      Weather pic div
-      {/* <InfoPanel /> */}
+      <InfoPanel />
     </div>
   );
 }
