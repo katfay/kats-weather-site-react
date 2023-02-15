3f2297da862065a88800d5d46ab7cb97045be8a6
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-02-15 15:28:02 +1100

    Pause troubleshooting background-image

--- src/WeatherPic.js
+++ src/WeatherPic.js
@@ -1,10 +1,5 @@
 import "./WeatherPic.css";
 
 export default function WeatherPic() {
-  return (
-    <div className="WeatherPic">
-      WeatherPic div
-      <div className="background-pic test-border">background-pic div</div>
-    </div>
-  );
+  return <div className="WeatherPic">WeatherPic div</div>;
 }
