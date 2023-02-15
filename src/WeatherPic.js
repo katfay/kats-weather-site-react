53559ebfa782f32e6786d8c28a4c991a9e8baade
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-02-15 15:55:08 +1100

    Start new branch for pic issue
    Solve pic issue

--- src/WeatherPic.js
+++ src/WeatherPic.js
@@ -1,5 +1,5 @@
 import "./WeatherPic.css";
 
 export default function WeatherPic() {
-  return <div className="WeatherPic">WeatherPic div</div>;
+  return <div className="WeatherPic">Weather pic div</div>;
 }
