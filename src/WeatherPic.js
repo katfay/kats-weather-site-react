d2f75c5f61ad9dac9b93a76ba21143deaf586f21
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-02-15 15:10:14 +1100

    Create container for background pic
    So that border of background pic sits within the main container (symmetry).

--- src/WeatherPic.js
+++ src/WeatherPic.js
@@ -1,5 +1,10 @@
 import "./WeatherPic.css";
 
 export default function WeatherPic() {
-  return <div className="WeatherPic">WeatherPic div</div>;
+  return (
+    <div className="WeatherPic">
+      WeatherPic div
+      <div className="background-pic test-border">background-pic div</div>
+    </div>
+  );
 }
