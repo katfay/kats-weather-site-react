24c75775e75e87c55b6a2bcdd55bc69601571476
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-02-15 18:46:00 +1100

    Call it a day

--- src/GeoInfo.js
+++ src/GeoInfo.js
@@ -1,5 +1,16 @@
 import "./GeoInfo.css";
 
 export default function GeoInfo() {
-  return <div className="Search">GeoInfo div</div>;
+  return (
+    <div className="GeoInfo">
+      <div>
+        In <span>GeoImage </span>
+        <span>placename</span> now it's <span>WeatherImage</span>{" "}
+        <span>temperature</span>
+      </div>
+      <div>
+        <span>day</span>, <span>time</span>
+      </div>
+    </div>
+  );
 }
--- src/Search.js
+++ src/Search.js
@@ -5,6 +5,9 @@ export default function Search() {
     <div className="Search">
       <form>
         <input type="search" placeholder="Enter a city ..." id="" />
+        <button type="submit" id="">
+          Search
+        </button>
       </form>
     </div>
   );
